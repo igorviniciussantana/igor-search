d0cf4d5c3864e35d71d11d9fc79f9ac802449688
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-04 22:19:04 -0400

    feat(input): finishsearch input styles

--- src/components/Input/styled.ts
+++ src/components/Input/styled.ts
@@ -1,12 +1,26 @@
 import styled from "styled-components";
 
 export const InputWrapper = styled.div`
+margin-top: 2rem;
+
+display: flex;
+justify-content: stretch;
+column-gap: 0.5rem;
+width: 60vw;
 
 `;
 
 
 export const Input = styled.input`
+height: 3.5rem;
+width: 100%;
+font-size: 1.5rem;
 
+padding: 0 1.5rem;
+
+::placeholder{
+    color: #C3C3C3;
+}
 `;
 
 export const Button = styled.button`
