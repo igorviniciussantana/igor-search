81244700bea4750fbafad5a928aeff4be0a76670
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-04 22:34:55 -0400

    feat: create card link component files

---
+++ src/components/CardLink/index.tsx
@@ -0,0 +1,12 @@
+import * as Styled from "./styled";
+import { links } from "../../data/links";
+import Image from "next/image";
+
+
+export default function CardLink() {
+  return (
+    <Styled.Card>
+
+    </Styled.Card>
+  );
+}
