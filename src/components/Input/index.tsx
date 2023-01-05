577407b9bc456691ecfbc98191f1504b0236f828
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-04 22:25:38 -0400

    feat(input): finishsearch button styles

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,12 +1,13 @@
 import * as Styled from "./styled";
 import { placeholders } from "../../data/placeholders";
+import Image from "next/image";
 
 
 export default function Input() {
   return (
     <Styled.InputWrapper>
       <Styled.Input placeholder={placeholders[Math.floor(Math.random() * placeholders.length)]}/>
-      <Styled.Button >Pesquisar</Styled.Button>
+      <Styled.Button ><Image src='./searchSvg.svg' width='32' alt='Ícone de busca' height='32'/></Styled.Button>
 
     </Styled.InputWrapper>
   );
--- src/components/Input/styled.ts
+++ src/components/Input/styled.ts
@@ -17,6 +17,14 @@ width: 100%;
 font-size: 1.5rem;
 
 padding: 0 1.5rem;
+border-radius: 0.25rem;
+border: none;
+
+
+
+:focus{
+    outline: 2px solid #7DE2D1;
+}
 
 ::placeholder{
     color: #C3C3C3;
@@ -24,5 +32,17 @@ padding: 0 1.5rem;
 `;
 
 export const Button = styled.button`
+border: none;
+background-color: #339989;
+border-radius: 0.25rem;
+padding: 0 2rem;
 
+transition: .5s;
+
+cursor: pointer;
+
+:hover{
+    background-color: #1F6A5F;
+    transition: .5s;
+}
 `;
