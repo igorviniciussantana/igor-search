36d5f8b19e77fbaa3b8051e4355c892c05a409f4
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-04 22:45:09 -0400

    feat: add search function

---
+++ src/components/CardLink/styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+margin-top: 2rem;
+
+display: flex;
+justify-content: stretch;
+column-gap: 0.5rem;
+min-width: 60vw;
+
+`;
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,14 +1,32 @@
 import * as Styled from "./styled";
 import { placeholders } from "../../data/placeholders";
 import Image from "next/image";
+import Link from "next/link";
 
+import { useState } from "react";
 
 export default function Input() {
+  const [search, setSearch] = useState("");
+
   return (
     <Styled.InputWrapper>
-      <Styled.Input placeholder={placeholders[Math.floor(Math.random() * placeholders.length)]}/>
-      <Styled.Button ><Image src='./searchSvg.svg' width='32' alt='Ícone de busca' height='32'/></Styled.Button>
-
+      <Styled.Input
+        placeholder={
+          placeholders[Math.floor(Math.random() * placeholders.length)]
+        }
+        onChange={(ev) => setSearch(ev.target.value)}
+      />
+      <Link href={"https://www.google.com/search?q=" + search}>
+        {" "}
+        <Styled.Button>
+          <Image
+            src="./searchSvg.svg"
+            width="32"
+            alt="Ícone de busca"
+            height="32"
+          />
+        </Styled.Button>
+      </Link>
     </Styled.InputWrapper>
   );
 }
