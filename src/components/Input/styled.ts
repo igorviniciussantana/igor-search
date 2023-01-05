577f6ff346bb744bcf25779df887d5e08ca71d2e
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-05 08:25:22 -0400

    style: auto format using prettier

--- src/components/Input/styled.ts
+++ src/components/Input/styled.ts
@@ -1,51 +1,47 @@
 import styled from "styled-components";
 
-interface FormProps{
-    onSubmit: (e: React.FormEvent<HTMLInputElement>) => void;
+interface FormProps {
+  onSubmit: (e: React.FormEvent<HTMLInputElement>) => void;
 }
 
 export const InputWrapper = styled.form<FormProps>`
-margin-top: 2rem;
+  margin-top: 2rem;
 
-display: flex;
-column-gap: 0.5rem;
-min-width: 60vw;
-
+  display: flex;
+  column-gap: 0.5rem;
+  min-width: 60vw;
 `;
 
-
 export const Input = styled.input`
-height: 3.5rem;
-width: 100%;
-font-size: 1.5rem;
+  height: 3.5rem;
+  width: 100%;
+  font-size: 1.5rem;
 
-padding: 0 1.5rem;
-border-radius: 0.25rem;
-border: none;
+  padding: 0 1.5rem;
+  border-radius: 0.25rem;
+  border: none;
 
+  :focus {
+    outline: 2px solid #7de2d1;
+  }
 
-
-:focus{
-    outline: 2px solid #7DE2D1;
-}
-
-::placeholder{
-    color: #C3C3C3;
-}
+  ::placeholder {
+    color: #c3c3c3;
+  }
 `;
 
 export const Button = styled.button`
-border: none;
-background-color: #339989;
-border-radius: 0.25rem;
-padding: 0 2rem;
+  border: none;
+  background-color: #339989;
+  border-radius: 0.25rem;
+  padding: 0 2rem;
 
-transition: 500ms;
+  transition: 500ms;
 
-cursor: pointer;
+  cursor: pointer;
 
-:hover{
-    background-color: #1F6A5F;
+  :hover {
+    background-color: #1f6a5f;
     transition: 500ms;
-}
+  }
 `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,30 @@
-import Head from 'next/head'
-import { Title, Input } from '@components/imports'
+import Head from "next/head";
+import { Title, Input } from "@components/imports";
 import { links } from "@data/links";
-import CardLink from '@components/CardLink';
-
-
+import CardLink from "@components/CardLink";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Igor Search | Pesquisar</title>
-        <meta name="description" content="Webpage personalizada para buscas no Google." />
+        <meta
+          name="description"
+          content="Webpage personalizada para buscas no Google."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-   <Title />
-   <Input />
+        <Title />
+        <Input />
 
-   <nav>
-{links.map(link =>{
-  return(
-    <CardLink img={link.img} title={link.title}/>
-  )
-})}
-   </nav>
+        <nav>
+          {links.map((link) => {
+            return <CardLink img={link.img} title={link.title} />;
+          })}
+        </nav>
       </main>
     </>
-  )
+  );
 }
